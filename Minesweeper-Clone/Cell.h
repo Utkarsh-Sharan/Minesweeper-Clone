871ade64f9d02ff5f78b3b4c3551903e15698605
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-10-21 12:04:47 +0530

    designed cell data

--- Minesweeper-Clone/Cell.h
+++ Minesweeper-Clone/Cell.h
@@ -1,1 +1,25 @@
 #pragma once
+
+namespace Cell
+{
+	class Cell
+	{
+	private:
+		bool is_mine;			//cell's a mine?
+		bool is_revealed;		//cell's revealed?
+		int adjacent_mines;		//number of adj mines to this cell.
+
+	public:
+		Cell() : is_mine(false), is_revealed(false), adjacent_mines(0) {}	//initializer list
+
+		void PlaceMine();
+
+		bool IsMine();
+		bool IsRevealed();
+
+		void Reveal();
+
+		int GetAdjacentMines();
+		void SetAdjacentMines(int mine_count);
+	};
+}
